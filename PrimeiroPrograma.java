1fd08e664faecb47cadd5a926e1ffd1f8fb4057f
Isadora Ferreira Barcella <isabarcella@outlook.com>
2021-05-25 14:12:03 -0300

    first commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+/**
+ * PrimeiroPrograma
+ */
+public class PrimeiroPrograma {
+
+  public static void main(String[] args) {
+    System.out.println("ola mundo");
+    System.out.println("godinho gotoso");
+  }
+}
